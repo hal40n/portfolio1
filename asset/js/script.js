192bcf23d936d323518f9ac2e3b71fd7d07cf0f0
Shoma Takeuchi <shomatakeuchi.info@gmail.com>
2023-05-22 01:23:02 +0900

    add slider

--- asset/js/script.js
+++ asset/js/script.js
@@ -65,3 +65,29 @@ $(function() {
     $('.l-header__menu').toggleClass('open');
   });
 });
+
+
+/* slider of tour contents */
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 10000,
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+      slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
